0a359cef0b9a00f7637cf73b2158d8c95114fbc6
Nicholas Flintham <nick@flinny.org>
2012-06-21 11:12:48 +0100

    Revert "BT change"
    This reverts commit 8b890bb2969f123ee8668a4865cd7737bad157c9.

--- vision-vendor-blobs.mk
+++ vision-vendor-blobs.mk
@@ -63,5 +63,4 @@ PRODUCT_COPY_FILES += \
     vendor/htc/vision/proprietary/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
     vendor/htc/vision/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     vendor/htc/vision/proprietary/libv8.so:/system/lib/libv8.so \
-    vendor/htc/vision/proprietary/libv8.so:obj/lib/libv8.so \
-    vendor/htc/vision/proprietary/btld:system/bin/btld
+    vendor/htc/vision/proprietary/libv8.so:obj/lib/libv8.so
