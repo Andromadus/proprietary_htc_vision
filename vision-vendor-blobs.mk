422addaf21df70419cd5e354c9b2fc602e050e70
Nicholas Flintham <nick@flinny.org>
2012-07-17 06:31:32 +0100

    add libwvm.so

--- vision-vendor-blobs.mk
+++ vision-vendor-blobs.mk
@@ -62,5 +62,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/vision/proprietary/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
     vendor/htc/vision/proprietary/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
     vendor/htc/vision/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/vision/proprietary/libwvm.so:/system/lib/libwvm.so \
     vendor/htc/vision/proprietary/libv8.so:/system/lib/libv8.so \
     vendor/htc/vision/proprietary/libv8.so:obj/lib/libv8.so
