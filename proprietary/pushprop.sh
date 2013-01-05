dbd2c5eece3798db09cfca2204b9ee8cb1a6cf8f
Nicholas Flintham <nick@flinny.org>
2013-01-05 18:25:43 +0000

    update pushprop.sh
    Change-Id: I7e28624283c6c085e956dc1972bccbd37d66365b

--- proprietary/pushprop.sh
+++ proprietary/pushprop.sh
@@ -4,10 +4,12 @@ adb push libGLESv1_CM_adreno200.so /system/lib/egl/libGLESv1_CM_adreno200.so
 adb push libGLESv2_adreno200.so /system/lib/egl/libGLESv2_adreno200.so
 adb push libq3dtools_adreno200.so /system/lib/egl/libq3dtools_adreno200.so
 adb push libGLESv2S3D_adreno200.so /system/lib/egl/libGLESv2S3D_adreno200.so
+adb push libplayback_adreno200.so /system/lib/egl/libplayback_adreno200.so
 adb push libC2D2.so /system/lib/libC2D2.so
 adb push libc2d2_z180.so /system/lib/libc2d2_z180.so
 adb push libgsl.so /system/lib/libgsl.so
 adb push libOpenVG.so /system/lib/libOpenVG.so
+adb push libOpenCL.so /system/lib/libOpenCL.so
 adb push libsc-a2xx.so /system/lib/libsc-a2xx.so
 adb push a225p5_pm4.fw /system/etc/firmware/a225p5_pm4.fw
 adb push a225_pfp.fw /system/etc/firmware/a225_pfp.fw
